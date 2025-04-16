d562b3615b1c98fdb50a0fc2b191ef3866e335ca
Frankozoalor <frankozoalor@gmail.com>
2025-04-16 21:00:18 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showForm(formid) {
+ document
+  .querySelectorAll(".form-box")
+  .forEach((form) => form.classList.remove("active"));
+ document.getElementById(formid).classList.add("active");
+}
